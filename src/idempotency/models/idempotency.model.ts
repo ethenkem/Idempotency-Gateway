16ec665ed17a978f9029fd7238d6b758f54cf7c0
ethenkem <ethenatx@gmail.com>
2026-03-03 10:53:01 +0000

    feat(idempotency-module): add idempotency model

--- src/idempotency/models/idempotency.model.ts
+++ src/idempotency/models/idempotency.model.ts
@@ -1,1 +1,24 @@
-export class Idempotency {}
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+@Entity()
+export class IdempotencyModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  idempotencyKey: string;
+
+  @Column()
+  requestHash: string;
+
+  @Column({ type: 'text', nullable: true })
+  responseData: string;
+
+  // stores the previous response status code
+  // to determine if the request is pending, completed, or failed
+  @Column({ nullable: true })
+  statusCode: number;
+
+  @Column()
+  status: 'pending' | 'completed' | 'failed';
+}
