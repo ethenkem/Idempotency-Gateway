1d62a31709b9245fff08b9d365b9d155921ce17b
ethenkem <ethenatx@gmail.com>
2026-03-03 10:32:19 +0000

    chore:set up sqlite configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PaymentModule } from './payment/payment.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.model{.ts,.js}'],
+      synchronize: true,
+    }),
+    PaymentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
