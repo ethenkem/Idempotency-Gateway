ce70543a5ed851cd4b5f5fe77b7a92fb030b0336
ethenkem <ethenatx@gmail.com>
2026-03-03 21:59:20 +0000

    refactor(idempotency-module): remove extra status checks

--- src/idempotency/idempotency.service.ts
+++ src/idempotency/idempotency.service.ts
@@ -2,6 +2,9 @@ import {
   BadRequestException,
   ConflictException,
   Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { IdempotencyModel } from './models/idempotency.model';
@@ -13,6 +16,7 @@ import { IdempotencyHelper } from './idempotency.helper';
 
 @Injectable()
 export class IdempotencyService {
+  private readonly logger = new Logger(IdempotencyService.name);
   constructor(
     @InjectRepository(IdempotencyModel)
     private idempotencyRepository: Repository<IdempotencyModel>,
@@ -20,72 +24,53 @@ export class IdempotencyService {
     private idempotencyHelper: IdempotencyHelper,
   ) {}
 
-  async executeOrReplay(
-    idempotencyKey: string,
-    body: ProcessPaymentDto,
-    processPayment: () => Promise<any>,
-  ) {
-    if (!idempotencyKey) {
-      throw new BadRequestException('Idempotency key is required');
-    }
+  async createIdempotencyRecord(idempotencyKey: string, body: ProcessPaymentDto): Promise<IdempotencyModel> {
     const requestHash = this.sharedService.hashRequestBody(body);
-    // get existing record if its exists;
-    // otherwise create new record
-    let record: IdempotencyModel | null =
-      await this.idempotencyRepository.findOne({
+    const record = this.idempotencyRepository.create({
+      idempotencyKey,
+      requestHash,
+      status: 'processing',
+    });
+    return await this.idempotencyRepository.save(record);
+  }
+
+  async updateIdempotencyRecord(record: IdempotencyModel, statusCode: number, responseData: any) {
+    record.statusCode = statusCode;
+    record.responseData = JSON.stringify(responseData);
+    record.status = 'completed';
+    await this.idempotencyRepository.save(record);
+  }
+
+  async findOneRecordByIdempotencyKey(idempotencyKey: string): Promise<IdempotencyModel> {
+    let record: IdempotencyModel | null = await this.idempotencyRepository.findOne({
         where: { idempotencyKey },
       });
 
-    if (!record) {
-      record = this.idempotencyRepository.create({
-        idempotencyKey,
-        requestHash,
-        status: 'processing',
-      });
-      await this.idempotencyRepository.save(record);
+    if (!record) throw new NotFoundException('Idempotency record not found');
+    return record;
+  }
 
-      // call and simulate processing delay
-      const result = await processPayment();
-      record.statusCode = result.statusCode;
-      record.responseData = JSON.stringify(result.body);
-      record.status = 'completed';
-      await this.idempotencyRepository.save(record);
-      return {
-        statusCode: 200,
-        body: result,
-        headers: null,
-      };
+  async executeOrReplay(idempotencyKey: string, body: ProcessPaymentDto): Promise<IdempotencyResponseDto> {
+    const requestHash = this.sharedService.hashRequestBody(body);
+    // get existing record if its exists;
+    let record: IdempotencyModel | null = await this.idempotencyRepository.findOne({where: { idempotencyKey },});
+      
+    if (!record) {
+      throw new NotFoundException('Idempotency record not found');
     }
 
     // 6. Duplicate key handling
+    // Different Request, Same Key (Fraud/Error Check) 
     if (record.requestHash !== requestHash) {
       throw new ConflictException(
         'Idempotency key already used for a different request body.',
       );
     }
 
-    if (record.status === 'completed') {
-      return {
+    return {
         statusCode: record.statusCode,
         headers: { 'X-Cache-Hit': 'true' },
         body: JSON.parse(record.responseData),
       };
-    }
-
-    if (record.status === 'processing') {
-      await this.idempotencyHelper.waitUntilProcessingCompletes(record.id);
-      record = await this.idempotencyRepository.findOne({
-        where: { id: record.id },
-      });
-      if (record?.status === 'completed') {
-        return {
-          statusCode: record.statusCode,
-          headers: { 'X-Cache-Hit': 'true' },
-          body: JSON.parse(record.responseData),
-        };
-      } else {
-        throw new BadRequestException('Previous request failed. Please retry.');
-      }
-    }
   }
 }
